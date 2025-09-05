375ee48c647744d4c3e58bb444ee57253347213e
Yuing <2248205150@qq.com>
2025-09-05 11:30:45 +0800

    init:初始化父子工程学习仓库，并编写一个接口

---
+++ spring-boot-quickstart/src/main/java/com/example/week1/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.week1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/list")
+    public List<String> getList() {
+        return List.of("aaa", "bbb", "ccc");
+    }
+}
